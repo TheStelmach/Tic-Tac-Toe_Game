94f7ab08da832210f66185dc721f80a619044f29
thestelmach <nazarii.mor@gmail.com>
2025-12-20 12:55:12 -0500

    modified:   main.lua 	deleted:    sprites/TIC-TAC-TOE.kra 	deleted:    sprites/brush-8262610.kra 	renamed:    sprites/noughts-6195771 (Copy).svg -> sprites/crosses/crosses_and_noughts.svg 	new file:   sprites/gear-575568_640.png~ 	new file:   sprites/gear.png 	deleted:    sprites/noughts-6195771.svg 	renamed:    sprites/stroke1.png -> sprites/stroke1.png~ 	deleted:    sprites/stroke2.png 	new file:   sprites/stroke2.png~ 	deleted:    sprites/stroke3.png 	modified:   sprites/stroke3.png~ 	renamed:    sprites/stroke4.png -> sprites/stroke4.png~ 	renamed:    sprites/stroke5.png -> sprites/stroke5.png~ 	renamed:    sprites/stroke6.png -> sprites/stroke6.png~ 	renamed:    sprites/brush-8262610.svg -> sprites/strokes/all_strokes.svg 	renamed:    sprites/stroke0.png -> sprites/strokes/stroke0.png 	new file:   sprites/strokes/stroke1.png 	new file:   sprites/strokes/stroke2.png 	new file:   sprites/strokes/stroke3.png 	new file:   sprites/strokes/stroke4.png 	new file:   sprites/strokes/stroke5.png 	new file:   sprites/strokes/stroke6.png

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@ function Love.load()
     crossTable = {"cross1", "cross2", "cross3"}
     noughtTable = {"nought1", "nought2", "nought3"}
 
+    gearIcon = Love.graphics.newImage("sprites/gear.png")
+
     crossImage = function()
         return Love.graphics.newImage("sprites/crosses/" .. crossTable[math.random(#crossTable)] .. ".png")
     end
@@ -24,13 +26,13 @@ function Love.load()
     scalingFactor = 1.2 -- figure image scaling factor
     imageScale = (scalingFactor * 512)  -- 512 is the original image size
 
-    strokes = {Love.graphics.newImage("sprites/stroke0.png"),
-               Love.graphics.newImage("sprites/stroke1.png"),
-               Love.graphics.newImage("sprites/stroke2.png"),
-               Love.graphics.newImage("sprites/stroke3.png"),
-               Love.graphics.newImage("sprites/stroke4.png"),
-               Love.graphics.newImage("sprites/stroke5.png"),
-               Love.graphics.newImage("sprites/stroke6.png"),} 
+    strokes = {Love.graphics.newImage("sprites/strokes/stroke0.png"),
+               Love.graphics.newImage("sprites/strokes/stroke1.png"),
+               Love.graphics.newImage("sprites/strokes/stroke2.png"),
+               Love.graphics.newImage("sprites/strokes/stroke3.png"),
+               Love.graphics.newImage("sprites/strokes/stroke4.png"),
+               Love.graphics.newImage("sprites/strokes/stroke5.png"),
+               Love.graphics.newImage("sprites/strokes/stroke6.png"),} 
 
     beepSound = Love.audio.newSource("sounds/beepSound.mp3", "static")
     gameStartSound = Love.audio.newSource("sounds/gameStarted.mp3", "static")
@@ -40,10 +42,25 @@ function Love.load()
 
     mainFont = Love.graphics.newFont("fonts/atop-font/Atop-R99O3.ttf", 50)
 
-    r_back, g_back, b_back, a_back = 255/255, 251/255, 219/255, 0
-    Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
-    r_line, g_line, b_line, a_line = 235/255, 216/255, 61/255, 1
-    r_stroke, g_stroke, b_stroke, a_stroke = 235/255, 216/255, 61/255, 0.7
+    colorPalette = "bright"
+    gridSize = "3x3"
+    difficulty = "normal"
+    gameMode = "continuous"
+    sound = "on"
+
+    if colorPalette == "dark" then
+        r_back, g_back, b_back, a_back = 20/255, 20/255, 40/255, 0
+        Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+        r_line, g_line, b_line, a_line = 235/255, 216/255, 61/255, 1
+        r_stroke, g_stroke, b_stroke, a_stroke = 235/255, 216/255, 61/255, 0.7
+    else
+        r_back, g_back, b_back, a_back = 255/255, 251/255, 219/255, 0
+        Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+        r_line, g_line, b_line, a_line = 235/255, 216/255, 61/255, 1
+        r_stroke, g_stroke, b_stroke, a_stroke = 235/255, 216/255, 61/255, 0.7
+        r_text, g_text, b_text, a_text = 235/255, 231/255, 189/255, 0.5
+    end
+    
 
 
     r_circle, g_circle, b_circle, a_circle = 1, 1, 1, 1
@@ -86,20 +103,55 @@ function Love.update()
         if moves >= 6 then
             if (cell[1] == cell[2] and cell[2] == cell[3] and cell[1] ~= "empty") then
                 winner = cell[1]
-            elseif (cell[4] == cell[5] and cell[5] == cell[6] and cell[4] ~= "empty") then
-                winner = cell[4]
-            elseif (cell[7] == cell[8] and cell[8] == cell[9] and cell[7] ~= "empty") then
-                winner = cell[7]
-            elseif (cell[1] == cell[4] and cell[4] == cell[7] and cell[1] ~= "empty") then
-                winner = cell[1]
-            elseif (cell[2] == cell[5] and cell[5] == cell[8] and cell[2] ~= "empty") then
-                winner = cell[2]
-            elseif (cell[3] == cell[6] and cell[6] == cell[9] and cell[3] ~= "empty") then
-                winner = cell[3]
-            elseif (cell[1] == cell[5] and cell[5] == cell[9] and cell[1] ~= "empty") then
-                winner = cell[1]
-            elseif (cell[3] == cell[5] and cell[5] == cell[7] and cell[3] ~= "empty") then
-                winner = cell[3]
+            end
+            if (cell[4] == cell[5] and cell[5] == cell[6] and cell[4] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[4]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[7] == cell[8] and cell[8] == cell[9] and cell[7] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[7]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[1] == cell[4] and cell[4] == cell[7] and cell[1] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[1]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[2] == cell[5] and cell[5] == cell[8] and cell[2] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[2]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[3] == cell[6] and cell[6] == cell[9] and cell[3] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[3]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[1] == cell[5] and cell[5] == cell[9] and cell[1] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[1]
+                else
+                    winner = "draw"
+                end
+            end
+            if (cell[3] == cell[5] and cell[5] == cell[7] and cell[3] ~= "empty") then
+                if winner == "unknown" then 
+                    winner = cell[3]
+                else
+                    winner = "draw"
+                end
             end
         end
 
@@ -121,15 +173,33 @@ function Love.draw()
         Love.graphics.draw(strokes[1], windowWidth * 0.075, windowHeight * 0.1, 0, windowWidth / (2*imageScale), windowHeight / (2*imageScale))
         Love.graphics.draw(strokes[6], windowWidth * 0.22, windowHeight * 0.6, 0, windowWidth / (3*imageScale), windowHeight / (3*imageScale))
         Love.graphics.draw(strokes[3], windowWidth * 0.22, windowHeight * 0.75, 0, windowWidth / (3*imageScale), windowHeight / (4*imageScale))
-        Love.graphics.setColor( r_stroke, g_stroke, b_stroke, 0.9 )
-        drawCenteredText(Love.graphics.newText(mainFont, "TIC-TAC-TOE"), windowWidth * 0.26, windowHeight * 0.25, 0, windowWidth / 600, windowHeight / 600,"white")
-        drawCenteredText(Love.graphics.newText(mainFont, "Play with Computer"), windowWidth * 0.24, windowHeight * 0.64, 0, windowWidth / 1050, windowHeight / 1050,"white")
-        drawCenteredText(Love.graphics.newText(mainFont, "Play with Friend"), windowWidth * 0.27, windowHeight * 0.765, 0, windowWidth / 1050, windowHeight / 1050,"white")
+        
+        if colorPalette == "bright" then
+            Love.graphics.setColor( r_text, g_text, b_text, a_text )
+            Love.graphics.draw(gearIcon, windowWidth * 0.9, windowHeight * 0.03, 0, windowWidth / (13*imageScale), windowHeight / (13*imageScale))
+        else
+            Love.graphics.setColor(10/255, 10/255, 40/255, 1)
+            Love.graphics.draw(gearIcon, windowWidth * 0.9, windowHeight * 0.03, 0, windowWidth / (13*imageScale), windowHeight / (13*imageScale))
+            Love.graphics.setColor( r_text, g_text, b_text, a_text )
+        end
+        drawCenteredText(Love.graphics.newText(mainFont, "TIC-TAC-TOE"), windowWidth * 0.26, windowHeight * 0.25, 0, windowWidth / 600, windowHeight / 600,"messageText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Play with Computer"), windowWidth * 0.24, windowHeight * 0.64, 0, windowWidth / 1050, windowHeight / 1050,"messageText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Play with Friend"), windowWidth * 0.27, windowHeight * 0.765, 0, windowWidth / 1050, windowHeight / 1050,"messageText")
         ------- remove to settings page later
-        drawCenteredText(Love.graphics.newText(mainFont, "made by: @TheStelmach"), windowWidth * 0.69, windowHeight * 0.97, 0, windowWidth / 2200, windowHeight / 2200,"violet")
         
 
     elseif gameStarted == true and gameOver == false then
+        if colorPalette == "dark" then
+            r_back, g_back, b_back, a_back = 20/255, 20/255, 40/255, 0
+            Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+            r_line, g_line, b_line, a_line = 255/255, 103/255, 77/255, 1
+            r_stroke, g_stroke, b_stroke, a_stroke = 235/255, 216/255, 61/255, 0.7
+        else
+            r_back, g_back, b_back, a_back = 255/255, 251/255, 219/255, 0
+            Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+            r_line, g_line, b_line, a_line = 20/255, 20/255, 40/255, 1
+            r_stroke, g_stroke, b_stroke, a_stroke = 20/255, 20/255, 40/255, 0.7
+        end
         Love.graphics.setColor( r_line, g_line, b_line, a_line )
         Love.graphics.line( cellWidth, 0, cellWidth, windowHeight)
         Love.graphics.line( 2 * cellWidth, 0, 2 * cellWidth, windowHeight)
@@ -145,7 +215,7 @@ function Love.draw()
                     Love.graphics.draw(figure[i].sprite, (figure[i].col - 1) * cellWidth + cellWidth * (scalingFactor - 1) / 2, 
                                             (figure[i].row - 1) * cellHeight + cellWidth * (scalingFactor - 1) / 2, 0, cellWidth / imageScale, cellHeight / imageScale)
                 elseif figure[i].player == "O" then
-                    cell[cellNumber] = "0"
+                    cell[cellNumber] = "O"
                     Love.graphics.setColor( r_circle, g_circle, b_circle, a_circle )
                     Love.graphics.draw(figure[i].sprite, (figure[i].col - 1) * cellWidth + cellWidth * (scalingFactor - 1) / 2, 
                                             (figure[i].row - 1) * cellHeight + cellWidth * (scalingFactor - 1) / 2, 0, cellWidth / imageScale, cellHeight / imageScale)
@@ -159,20 +229,33 @@ function Love.draw()
         Love.graphics.draw(strokes[4], windowWidth * 0.22, windowHeight * 0.6, 0, windowWidth / (3*imageScale), windowHeight / (3*imageScale))
         Love.graphics.draw(strokes[5], windowWidth * 0.08, windowHeight * 0.8, 0, windowWidth / (2*imageScale), windowHeight / (3*imageScale))
         if winner == "X" then
-            drawCenteredText(Love.graphics.newText(mainFont, "CROSS WINS!"), windowWidth * 0.28, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"white")
+            drawCenteredText(Love.graphics.newText(mainFont, "CROSS WINS!"), windowWidth * 0.28, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"messageText")
         elseif winner == "O" then
-            drawCenteredText(Love.graphics.newText(mainFont, "NOUGHT WINS!"), windowWidth * 0.28, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"white")
+            drawCenteredText(Love.graphics.newText(mainFont, "NOUGHT WINS!"), windowWidth * 0.26, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"messageText")
         else
-            drawCenteredText(Love.graphics.newText(mainFont, "IT'S A DRAW!"), windowWidth * 0.28, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"white")
+            drawCenteredText(Love.graphics.newText(mainFont, "IT'S A DRAW!"), windowWidth * 0.28, windowHeight * 0.3, 0, windowWidth / 700, windowHeight / 700,"messageText")
         end
 
-        drawCenteredText(Love.graphics.newText(mainFont, "Back to Main Menu"), windowWidth * 0.26, windowHeight * 0.635, 0, windowWidth / 1000, windowHeight / 1000,"white")
-        drawCenteredText(Love.graphics.newText(mainFont, "Click to Restart"), windowWidth * 0.28, windowHeight * 0.835, 0, windowWidth / 1000, windowHeight / 1000,"white")
+        drawCenteredText(Love.graphics.newText(mainFont, "Back to Main Menu"), windowWidth * 0.26, windowHeight * 0.635, 0, windowWidth / 1000, windowHeight / 1000,"messageText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Click to Restart"), windowWidth * 0.28, windowHeight * 0.835, 0, windowWidth / 1000, windowHeight / 1000,"messageText")
         
     else
-
-        drawCenteredText(Love.graphics.newText(mainFont, "made by: @TheStelmach"), windowWidth * 0.69, windowHeight * 0.97, 0, windowWidth / 2200, windowHeight / 2200,"black")
+        -- gameStarted == false and gameOver == true
+        drawCenteredText(Love.graphics.newText(mainFont, "Settings"), windowWidth * 0.35, windowHeight * 0.15, 0, windowWidth / 800, windowHeight / 800,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Color Palette: " .. colorPalette), windowWidth * 0.15, windowHeight * 0.42, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Grid Size: " .. gridSize), windowWidth * 0.15, windowHeight * 0.49, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Difficulty: " .. difficulty), windowWidth * 0.15, windowHeight * 0.56, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Game mode: " .. gameMode), windowWidth * 0.15, windowHeight * 0.63, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Sound: " .. sound), windowWidth * 0.15, windowHeight * 0.70, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Return "), windowWidth * 0.15, windowHeight * 0.85, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "made by: @TheStelmach"), windowWidth * 0.69, windowHeight * 0.97, 0, windowWidth / 2200, windowHeight / 2200,"settingsText")
         
+-- settings page will contain:
+-- color mode dark vs bright
+-- sound on/off
+-- difficulty level (for playing vs computer) simple, normal, impossible
+-- playing mode (continuous vs limited moves)
+-- grid size (3x3, 4x4, 5x5)
     end
     
     screenUpdated = true
@@ -181,19 +264,19 @@ end
 function makeBoard()
 
     if gameStarted == false and gameOver == false then
-        if mouseX >= windowWidth * 0.22 and mouseX <= windowWidth * 0.55 and
+        if mouseX >= windowWidth * 0.2 and mouseX <= windowWidth * 0.8 and
            mouseY >= windowHeight * 0.6 and mouseY <= windowHeight * 0.68 then
             print("Play with Computer - Not implemented yet!")
             Love.audio.play(beepSound)
-        elseif mouseX >= windowWidth * 0.22 and mouseX <= windowWidth * 0.55 and
+        elseif mouseX >= windowWidth * 0.2 and mouseX <= windowWidth * 0.8 and
             mouseY >= windowHeight * 0.75 and mouseY <= windowHeight * 0.83 then
-            print("Play with Friend - Starting Game!")
             gameStarted = true
             Love.audio.play(gameStartSound)
             sleep(0.5)  -- Simple debounce to prevent multiple clicks
-        else
-            print("Click on one of the options to start the game.")
+        elseif mouseX >= windowWidth * 0.9 and mouseY <= windowHeight * 0.1 then
+            gameOver = true
             Love.audio.play(beepSound)
+        else
         end
 
     elseif gameStarted == true and gameOver == false then
@@ -273,7 +356,69 @@ function makeBoard()
             sleep(0.5)  -- Simple debounce to prevent multiple clicks
         end
     else
+        if mouseY >= windowHeight * 0.41 and mouseY <= windowHeight * 0.47 then
+            print("Palette changed")
+            if colorPalette == "bright" then
+                colorPalette = "dark"
+                r_back, g_back, b_back, a_back = 20/255, 20/255, 40/255, 0
+                Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+            else
+                colorPalette = "bright"
+                r_back, g_back, b_back, a_back = 255/255, 251/255, 219/255, 0
+                Love.graphics.setBackgroundColor(r_back, g_back, b_back, a_back)
+            end
+            Love.audio.play(beepSound)
+        elseif mouseY >= windowHeight * 0.48 and mouseY <= windowHeight * 0.54 then
+            print("Grid size changed")
+            if gridSize == "3x3" then
+                gridSize = "4x4"
+            elseif gridSize == "4x4" then
+                gridSize = "5x5"
+            else
+                gridSize = "3x3"
+            end
+            Love.audio.play(beepSound)
+        elseif mouseY >= windowHeight * 0.55 and mouseY <= windowHeight * 0.61 then
+            print("Difficulty changed")
+            if difficulty == "simple" then
+                difficulty = "normal"
+            elseif difficulty == "normal" then
+                difficulty = "impossible"
+            else
+                difficulty = "simple"
+            end
+            Love.audio.play(beepSound)
+        elseif mouseY >= windowHeight * 0.62 and mouseY <= windowHeight * 0.68 then
+            print("Game mode changed")
+            if gameMode == "continuous" then
+                gameMode = "limited moves"
+            else
+                gameMode = "continuous"
+            end
+            Love.audio.play(beepSound)
+        elseif mouseY >= windowHeight * 0.69 and mouseY <= windowHeight * 0.75 then
+            print("Sound toggled")
+            if sound == "on" then
+                sound = "off"
+                Love.audio.setVolume(0)
+            else
+                sound = "on"
+                Love.audio.setVolume(1)
+            end 
+            Love.audio.play(beepSound)
+        elseif mouseY >= windowHeight * 0.84 and mouseY <= windowHeight * 0.9 then
+            print("Returning to Main Menu")
+            gameOver = false
+            Love.audio.play(beepSound)
+        end
 
+        drawCenteredText(Love.graphics.newText(mainFont, "Color Palette: " .. colorPalette), windowWidth * 0.15, windowHeight * 0.42, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Grid Size: " .. gridSize), windowWidth * 0.15, windowHeight * 0.49, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Difficulty: " .. difficulty), windowWidth * 0.15, windowHeight * 0.56, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Game mode: " .. gameMode), windowWidth * 0.15, windowHeight * 0.63, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Sound: " .. sound), windowWidth * 0.15, windowHeight * 0.70, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        drawCenteredText(Love.graphics.newText(mainFont, "Return "), windowWidth * 0.15, windowHeight * 0.85, 0, windowWidth / 1000, windowHeight / 1000,"settingsText")
+        
     end
 end
 
@@ -295,9 +440,9 @@ function drawCenteredText(text, x, y, r, sx, sy, color)
         Love.graphics.setColor( 235/255, 216/255, 61/255, 0.75)
     elseif color == "black" then
         Love.graphics.setColor( 20/255, 20/255, 40/255, 1)
-    elseif color == "violet" then
+    elseif color == "settingsText" then
         Love.graphics.setColor( 119/255, 118/255, 188/255, 0.75)
-    elseif color == "white" then
+    elseif color == "messageText" then
 	    Love.graphics.setColor( r_back, g_back, b_back, 0.75)
     else
         Love.graphics.setColor( 0, 0, 1, 1)
@@ -307,4 +452,8 @@ function drawCenteredText(text, x, y, r, sx, sy, color)
 end
 
 
-FIX WINNING LOGIC AND IMPLEMENT A SETTINGS MENU
+
+-- TO DO LIST:
+-- DARK/BRIGHT COLOR MODE TOGGLE IN SETTINGS
+-- IMPLEMENT AI FOR PLAYING VS COMPUTER 
+-- ONLINE MULTIPLAYER (USING SOCKETS)
